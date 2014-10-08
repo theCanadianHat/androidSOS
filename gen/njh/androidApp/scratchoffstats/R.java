6b462b45a066a7d585d54edbc85593beb1003782
Noah Herron <jayssbronze@gmail.com>
2014-10-08 12:46:31 -0400

    Add EditTexts and tweak the UI

--- gen/njh/androidApp/scratchoffstats/R.java
+++ gen/njh/androidApp/scratchoffstats/R.java
@@ -1091,10 +1091,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f050042;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int buttonID=0x7f050041;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1133,14 +1134,19 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int spentID=0x7f05003c;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int ticketValue=0x7f05003f;
+        public static final int ticketWin=0x7f050040;
+        public static final int ticketsID=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
+        public static final int wonID=0x7f05003d;
     }
     public static final class integer {
         /**  The maximum number of action buttons that should be permitted within
@@ -1239,13 +1245,16 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
-        public static final int buttonText=0x7f0a0013;
-        public static final int hello_world=0x7f0a000e;
-        public static final int spent=0x7f0a0010;
-        public static final int tickets=0x7f0a0012;
-        public static final int won=0x7f0a0011;
+        public static final int buttonText=0x7f0a0016;
+        public static final int spentText=0x7f0a000f;
+        public static final int ticketValueEditText=0x7f0a0013;
+        public static final int ticketValueText=0x7f0a0012;
+        public static final int ticketWinEditText=0x7f0a0015;
+        public static final int ticketWinText=0x7f0a0014;
+        public static final int ticketsText=0x7f0a0011;
+        public static final int wonText=0x7f0a0010;
     }
     public static final class style {
         /** 
